594ca9d8bb764db18320a8c7b4aeb0d559a56218
Alamsyah191 <010901alam@gmail.com>
2026-07-28 22:34:30 +0700

    fix: correct logo text from 'tES' to 'Skatra ERP' in AppLogo component

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,7 +3,7 @@
         <img src="/logo.png" alt="Logo" class="h-8 w-8 object-contain" />
         <div class="ml-1 grid flex-1 text-left text-sm">
             <span class="mb-0.5 truncate leading-tight font-semibold"
-                >tES</span
+                >Skatra ERP</span
             >
         </div>
     </div>
